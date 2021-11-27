a4f0105f9b0f96f7012bcb5936e119b8fc5212fa
Max Zhilin <max_zhilin@mail.ru>
2021-11-27 21:06:31 +0300

    Stage 6 completed

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -4,42 +4,92 @@ import java.util.Scanner
 import java.util.stream.Collectors
 
 fun main() {
+    val calc = Calculator()
     val scanner = Scanner(System.`in`)
     do {
         val line = scanner.nextLine()
-        when (line) {
-            "/help" -> println("The program calculates the sum and difference of numbers")
-            "/exit" -> println("Bye!")
-            else -> {
-                if (line.isNotEmpty() && line.first() == '/') println("Unknown command")
-                else try {
-                    Scanner(line)
-                        .tokens()
-                        .collect(Collectors.toList())
-                        .reduceWithSignOrNull { sum, sign, it -> sum + sign * it }
-                        ?.let { println(it) }
-                } catch (e: Exception) {
-                    println("Invalid expression")
-                }
+        if (line.isBlank()) continue
+        else if (line.first() == '/') {
+            when (line) {
+                "/help" -> println("The program calculates the sum and difference of numbers")
+                "/exit" -> println("Bye!")
+                else -> println("Unknown command")
             }
-        }
+        } else
+            try {
+                if (line.contains('=')) {
+                    val assignment = line.split('=', limit = 2)
+                    val (identifier, expression) = assignment
+                    calc.assignExpression(identifier, expression)
+                } else {
+                    println(calc.evaluateExpression(line))
+                }
+            } catch (e: MyException) {
+                println(e.message)
+            }
     } while(line != "/exit")
 }
 
-fun Iterable<String>.reduceWithSignOrNull(operation: (acc: Int, sign: Int, Int) -> Int): Int? {
-    val iterator = this.iterator()
-    if (!iterator.hasNext()) return null
-    var accumulator: Int = iterator.next().parseToInt()
-    while (iterator.hasNext()) {
-        val sign = iterator.next().foldToSign()
-        accumulator = operation(accumulator, sign, iterator.next().parseToInt())
+class Calculator {
+
+    private val map = mutableMapOf<String, Int>()
+
+    fun evaluateExpression(expression: String): Int {
+        require(expression.isNotBlank())    // it is not necessary, but let it be for clearance
+
+        val iterator = Scanner(expression)
+            .tokens()   // are strings with no whitespaces
+            .collect(Collectors.toList())
+            .iterator()
+
+        try {
+            var accumulator: Int = evalWithUnarySign(iterator.next())
+
+            while (iterator.hasNext()) {
+                val sign = iterator.next().foldToSign()
+                accumulator += sign * evalWithUnarySign(iterator.next())
+            }
+
+            return accumulator
+
+        } catch (e: UnknownVariable) {  // only this exception rises as is
+            throw UnknownVariable()
+        } catch (e: Exception) {        // all other ones convert to special exception
+            throw InvalidExpression()
+        }
     }
-    return accumulator
+
+    private fun evalWithUnarySign(token: String): Int {
+        return when (token.first()) {
+            '+' -> eval(token.drop(1))
+            '-' -> -eval(token.drop(1))
+            else -> eval(token)
+        }
+    }
+
+    private fun eval(token: String): Int {
+        return if (token.matches(Regex("[a-zA-Z]+"))) {
+            map[token] ?: throw UnknownVariable()
+        } else token.toInt()
+    }
+
+    fun assignExpression(identifier: String, expression: String) {
+        val identifierName = identifier.trim()
+        if (!identifierName.matches(Regex("[a-zA-Z]+"))) throw InvalidIdentifier()
+        try {
+            val value = evaluateExpression(expression)
+            map[identifierName] = value
+        } catch (e: InvalidExpression) {
+            throw InvalidAssignment()   // this exception is only from assignment
+        }
+    }
 }
 
-fun String.parseToInt(): Int {
-    return if (first() == '+') substring(1).toInt() else toInt()
-}
+open class MyException(override val message: String): Exception(message)
+class InvalidIdentifier : MyException("Invalid identifier")
+class InvalidAssignment : MyException("Invalid assignment")
+class UnknownVariable : MyException("Unknown variable")
+class InvalidExpression : MyException("Invalid expression")
 
 fun String.foldToSign(): Int {
     var sign = 1
