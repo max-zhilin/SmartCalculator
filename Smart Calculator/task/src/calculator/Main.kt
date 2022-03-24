ce38506b9cf62898379b0a1ca663418de166f896
Max Zhilin <max_zhilin@mail.ru>
2022-03-24 03:08:45 +0300

    Stage 7 completed

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,7 +1,7 @@
 package calculator
 
+import java.math.BigInteger
 import java.util.Scanner
-import java.util.stream.Collectors
 
 fun main() {
     val calc = Calculator()
@@ -17,12 +17,16 @@ fun main() {
             }
         } else
             try {
-                if (line.contains('=')) {
-                    val assignment = line.split('=', limit = 2)
-                    val (identifier, expression) = assignment
-                    calc.assignExpression(identifier, expression)
+                val tokensList = tokenize(line)
+                if (tokensList.size >= 2 && tokensList[1].type == TokenType.DELIMITER && tokensList[1].value == "=") {
+                    val identifier = tokensList.first().value
+                    val expression = Parser(tokensList.subList(2, tokensList.size)).parseToRPN()
+                    val value = calc.evaluateExpression(expression)
+                    calc.assignVariable(identifier, value)
                 } else {
-                    println(calc.evaluateExpression(line))
+                    val expression = Parser(tokensList).parseToRPN()
+                    val value = calc.evaluateExpression(expression)
+                    println(value)
                 }
             } catch (e: MyException) {
                 println(e.message)
@@ -34,71 +38,237 @@ class Calculator {
 
     private val map = mutableMapOf<String, Int>()
 
-    fun evaluateExpression(expression: String): Int {
-        require(expression.isNotBlank())    // it is not necessary, but let it be for clearance
-
-        val iterator = Scanner(expression)
-            .tokens()   // are strings with no whitespaces
-            .collect(Collectors.toList())
-            .iterator()
+    fun evaluateExpression(expression: List<Element>): Int {
+        val iterator = expression.iterator()
+        val stack = Stack<Int>()
 
         try {
-            var accumulator: Int = evalWithUnarySign(iterator.next())
-
             while (iterator.hasNext()) {
-                val sign = iterator.next().foldToSign()
-                accumulator += sign * evalWithUnarySign(iterator.next())
+                when (val element = iterator.next()) {
+                    is Element.Number -> stack.push(element.value)
+                    is Element.Identifier -> stack.push(eval(element.name))
+                    is Element.Negate -> stack.push( - stack.pop())
+                    is Element.Minus -> {
+                        val second = stack.pop() // second is first
+                        stack.push(stack.pop() - second)
+                    }
+                    is Element.Plus -> stack.push(stack.pop() + stack.pop())
+                    is Element.Times -> stack.push(stack.pop() * stack.pop())
+                    is Element.Div -> {
+                        val second = stack.pop() // second is first
+                        stack.push(stack.pop() / second)
+                    }
+                    is Element.Power -> {
+                        val second = stack.pop() // second is first
+                        stack.push(pow(stack.pop(), second))
+                    }
+                    is Element.Parenthesis -> throw InvalidExpression() // it will never strike, for exhaustive when only
+                }
             }
 
-            return accumulator
+            val result = stack.pop()
 
-        } catch (e: UnknownVariable) {  // only this exception rises as is
-            throw UnknownVariable()
+            if (stack.isEmpty())
+                return result
+            else throw InvalidExpression()
+
+//        } catch (e: UnknownVariable) {  // only this exception rises as is
+//            throw UnknownVariable()
         } catch (e: Exception) {        // all other ones convert to special exception
             throw InvalidExpression()
         }
     }
 
-    private fun evalWithUnarySign(token: String): Int {
-        return when (token.first()) {
-            '+' -> eval(token.drop(1))
-            '-' -> -eval(token.drop(1))
-            else -> eval(token)
-        }
+    private fun eval(identifier: String): Int {
+        return if (identifier.matches(Regex("[a-zA-Z]+"))) {
+            map[identifier] ?: throw UnknownVariable()
+        } else identifier.toInt()
     }
 
-    private fun eval(token: String): Int {
-        return if (token.matches(Regex("[a-zA-Z]+"))) {
-            map[token] ?: throw UnknownVariable()
-        } else token.toInt()
-    }
-
-    fun assignExpression(identifier: String, expression: String) {
-        val identifierName = identifier.trim()
-        if (!identifierName.matches(Regex("[a-zA-Z]+"))) throw InvalidIdentifier()
-        try {
-            val value = evaluateExpression(expression)
-            map[identifierName] = value
-        } catch (e: InvalidExpression) {
-            throw InvalidAssignment()   // this exception is only from assignment
-        }
+    fun assignVariable(identifier: String, value: Int) {
+        if (!identifier.matches(Regex("[a-zA-Z]+"))) throw InvalidIdentifier()
+        map[identifier] = value
     }
 }
 
 open class MyException(override val message: String): Exception(message)
 class InvalidIdentifier : MyException("Invalid identifier")
-class InvalidAssignment : MyException("Invalid assignment")
 class UnknownVariable : MyException("Unknown variable")
 class InvalidExpression : MyException("Invalid expression")
 
-fun String.foldToSign(): Int {
-    var sign = 1
-    for (c in this) {
-        sign *= when(c) {
-            '+' -> 1
-            '-' -> -1
-            else -> throw IllegalArgumentException()
+class Stack<T> {
+    private val list = mutableListOf<T>()
+
+    fun push(element: T) {
+        list.add(element)
+    }
+
+    fun pop(): T = list.removeAt(list.lastIndex)
+
+    fun isEmpty() = list.isEmpty()
+ 
+    fun onTop(): T = list[list.lastIndex]
+}
+
+enum class TokenType {
+    IDENTIFIER, NUMBER, DELIMITER
+}
+class Token(val type: TokenType, val value: String)
+
+fun tokenize(s: String) : List<Token> {
+    val tokens = mutableListOf<Token>()
+
+    val scanner = Scanner(s)
+    scanner.useDelimiter("")    // read one char
+    var eof = false // End of file
+    var c = scanner.next().first()
+    while (!eof) {
+
+        when (c) {
+            ' ' -> {
+                if (scanner.hasNext()) c = scanner.next().first()
+                else eof = true
+            }
+            in 'a'..'z', in 'A'..'Z' -> {
+                var buffer = c.toString()
+                do {
+                    if (scanner.hasNext()) c = scanner.next().first()
+                    else eof = true
+
+                    val isLetter = !eof && c.isLetter()
+                    if (isLetter)
+                        buffer += c
+                    else {
+                        tokens.add(Token(TokenType.IDENTIFIER, buffer))
+                    }
+                } while (isLetter)
+            }
+            in '0'..'9' -> {
+                var buffer = c.toString()
+                do {
+                    if (scanner.hasNext()) c = scanner.next().first()
+                    else eof = true
+
+                    val isDigit = !eof && c.isDigit()
+                    if (isDigit)
+                        buffer += c
+                    else {
+                        tokens.add(Token(TokenType.NUMBER, buffer))
+                    }
+                } while (isDigit)
+            }
+            '+','-' -> {
+                var sign = c.toString()
+                do {
+                    if (scanner.hasNext()) c = scanner.next().first()
+                    else eof = true
+
+                    val isSign = !eof && (c == '+' || c == '-')
+                    if (isSign) {
+                        if (c == '-')
+                            sign = if (sign == "-") "+" else "-"
+                    }
+                    else {
+                        tokens.add(Token(TokenType.DELIMITER, sign))
+                    }
+                } while (isSign)
+            }
+            '=','*','/','(',')','^' -> {
+                tokens.add(Token(TokenType.DELIMITER, c.toString()))
+                if (scanner.hasNext()) c = scanner.next().first()
+                else eof = true
+            }
         }
     }
-    return sign
+
+    return tokens
+}
+
+sealed class Element(val priority: Int) {
+    class Identifier(val name: String) : Element(0)
+    class Number(val value: Int) : Element(0)
+    object Parenthesis : Element(0)
+    object Plus : Element(1)
+    object Minus : Element(1)
+    object Times : Element(2)
+    object Div : Element(2)
+    object Power : Element(3)
+    object Negate : Element(4)
+}
+
+class Parser(tokens: List<Token>) {
+    val iterator = tokens.iterator()
+    private val stack = Stack<Element>()
+    private val list = mutableListOf<Element>()
+    fun parseToRPN(): List<Element> {
+
+        var unaryAllowed = true // at start of after '('
+        while (iterator.hasNext()) {
+            val token = iterator.next()
+            when(token.type) {
+                TokenType.NUMBER -> list.add(Element.Number(token.value.toInt()))
+                TokenType.IDENTIFIER -> list.add(Element.Identifier(name = token.value))
+                TokenType.DELIMITER -> {
+                    when (token.value) {
+                        "-" -> {
+                            if (unaryAllowed)
+                                stack.push(Element.Negate)
+                            else {
+                                popByPriority(1)
+                                stack.push(Element.Minus)
+                            }
+                        }
+                        "+" -> {
+                            popByPriority(1)
+                            stack.push(Element.Plus)
+                        }
+                        "*" -> {
+                            popByPriority(2)
+                            stack.push(Element.Times)
+                        }
+                        "/" -> {
+                            popByPriority(2)
+                            stack.push(Element.Div)
+                        }
+                        "^" -> {
+                            popByPriority(3)
+                            stack.push(Element.Power)
+                        }
+                        "(" -> {
+                            stack.push(Element.Parenthesis)
+                        }
+                        ")" -> {
+                            popByParenthesis()
+                        }
+                        else -> throw InvalidExpression()
+                    }
+                }
+            }
+            unaryAllowed = token.type == TokenType.DELIMITER && token.value == "("
+        }
+        while (!stack.isEmpty()) {
+            if (stack.onTop() != Element.Parenthesis)
+                list.add(stack.pop())
+            else throw InvalidExpression()
+        }
+
+        return list
+    }
+
+    private fun popByPriority(priority: Int) {
+        while (!stack.isEmpty() && stack.onTop() != Element.Parenthesis && stack.onTop().priority >= priority)
+            list.add(stack.pop())
+    }
+    private fun popByParenthesis() {
+        while (!stack.isEmpty() && stack.onTop() != Element.Parenthesis)
+            list.add(stack.pop())
+
+        if (!stack.isEmpty() && stack.onTop() == Element.Parenthesis)
+            stack.pop()
+        else throw InvalidExpression()
+    }
+}
+
+fun pow(n: Int, exp: Int): Int {
+    return BigInteger.valueOf(n.toLong()).pow(exp).toInt()
 }
